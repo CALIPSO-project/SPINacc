dd709e929f474cb12c2c37417408c24fb446fa88
Daniel S. Goll <79981678+dsgoll123@users.noreply.github.com>
2022-12-23 08:47:59 +0100

    Update genMask.py

--- Tools/genMask.py
+++ Tools/genMask.py
@@ -19,8 +19,8 @@ from Tools import *
 ##@param[in]   packdata               packaged data
 ##@param[in]   varlist                list of variables, including name of source files, variable names, etc.
 ##@param[in]   thres                  threshold for PFT fraction to be defined as valid pixel
-##@retval      PFT_mask               PFT mask where PFT fraction >0.01
-##@retval      PFT_mask_lai           PFT mask where LAI >0
+##@retval      PFT_mask               PFT mask where PFT fraction < thres
+##@retval      PFT_mask_lai           PFT mask where LAI <0
 def PFT(packdata,varlist,thres):
   f=Dataset(varlist['PFTmask']['sourcefile'],'r')
   mkk=f[varlist['PFTmask']['var']][-1].filled(np.nan)

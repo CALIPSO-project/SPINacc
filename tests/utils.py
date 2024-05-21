bd891efe31e52b49a713495d4cae5d02aff0a9c1
tztsai <1213815284@qq.com>
2024-05-21 15:54:26 +0200

    Replace equal assertions with np.allclose
    (cherry picked from commit 5288795d4141e365d6a6321857138ec6b163ba63)

--- tests/utils.py
+++ tests/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 def compare_npy_files(file1, file2):
     a = np.load(file1, allow_pickle=True)
     b = np.load(file2, allow_pickle=True)
-    assert np.all(a == b), f"The contents of {file1} and {file2} are different."
+    assert np.allclose(a, b), f"The contents of {file1} and {file2} are different."
 
 
 def compare_nc_files(file1_path, file2_path):
@@ -27,9 +27,9 @@ def compare_nc_files(file1_path, file2_path):
     for variable in set(nc1.variables.keys()) & set(nc2.variables.keys()):
         values1 = nc1[variable][:]
         values2 = nc2[variable][:]
-        assert (
-            values1 == values2
-        ).all(), f"Variable '{variable}' values are different."
+        assert np.allclose(
+            values1, values2
+        ), f"Variable '{variable}' values are different."
 
     # Close the netCDF files
     nc1.close()

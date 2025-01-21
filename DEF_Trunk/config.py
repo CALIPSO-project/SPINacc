c5a861056cb3ff615ede5f345120fce3cc7dbd52
Matt Archer <ma595@cam.ac.uk>
2025-01-21 14:22:32 +0000

    Add more to config.py

--- DEF_Trunk/config.py
+++ DEF_Trunk/config.py
@@ -2,15 +2,13 @@
 
 logfile = "log.MLacc_Trunk"
 tasks = [
-    # 2,
+    1,
+    2,
+    # 3,
     4,
     5,
 ]  # 1=test clustering, 2=clustering, 3=compress forcing, 4=ML, 5=evaluation
-results_dir = "./EXE_DIR/"
-reference_dir = "/home/surface10/mrasolon/files_for_zenodo/reference/EXE_DIR/"
-start_from_scratch = False
-take_year_average = False
-smote_bat = False
+
 kmeans_clusters = 4
 max_kmeans_clusters = 9
 random_seed = 1000
@@ -22,6 +20,18 @@ algorithms = [
     # "ridge",
     "best",
 ]  # bt: BaggingTrees, rf: RandomForest, nn: MLPRegressor, gbm: XGBRegressor, lasso: Lasso, best: SelectBestModel
+
+start_from_scratch = False
+
+# To modify:
+take_year_average = False  # Performance improved when set to True
+smote_bat = True
+model_out = False  # Optional
+parallel = True  # True by default
+
+# Output and testing
+results_dir = "./EXE_DIR/"
+reference_dir = "/home/surface10/mrasolon/files_for_zenodo/reference/EXE_DIR/"
 leave_one_out_cv = False
 repro_test_task_1 = False
 repro_test_task_2 = False

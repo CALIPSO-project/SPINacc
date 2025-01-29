b07d0ef155625e512293bc73881d6466cd66e7d1
Matt Archer <ma595@cam.ac.uk>
2025-01-29 12:41:43 +0000

    Add a better comment in select_best_model function

--- Tools/train.py
+++ Tools/train.py
@@ -303,7 +303,9 @@ def training_bat(
 def select_best_model(X, Y, estimators, SW):
     """
     Select the best machine learning model based on the R2 score.
-
+    Uses a subset of the training data to make a judgement on the 'best'
+    performing algorithm. This means it may perform worse on some variables
+    compared to `bt` or other algorithms.
 
     Args:
         X (pandas.DataFrame): Features.

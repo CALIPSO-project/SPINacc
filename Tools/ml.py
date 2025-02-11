545adaf4046a9966cbb6362958d9aa165d4433b0
Matt Archer <ma595@cam.ac.uk>
2025-02-11 19:19:24 +0000

    Switched to fork

--- Tools/ml.py
+++ Tools/ml.py
@@ -469,7 +469,7 @@ def ml_loop(
     if parallel:
         import multiprocessing
 
-        # multiprocessing.set_start_method("spawn", force=True)
+        multiprocessing.set_start_method("fork", force=True)
         with ProcessPoolExecutor(max_workers=1) as executor:
             # Call the MLmap_multidim function with the arguments in inputs
             # Inputs is a list of tuples, each tuple is the arguments for the function
